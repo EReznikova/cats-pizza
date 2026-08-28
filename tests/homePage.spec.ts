2bebf40b75c3692331f8a713904fcfbd5257e750
Ereznikova <ice553@ya.ru>
2026-08-28 23:21:43 +0300

    add Check Card list items test

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -5,3 +5,11 @@ test('Check header', async ({ page }) => {
   const header = page.getByTestId('homePageHeader');
   await expect(header).toBeVisible();
 });
+
+test('Check card list items', async ({ page }) => {
+  page.goto('');
+  const firstCard = page.getByTestId('catCard_0');
+  const cardListItems = page.getByTestId(/catCard/);
+  await expect(firstCard).toBeVisible();
+  expect(await cardListItems.count()).toBeGreaterThan(0);
+});
